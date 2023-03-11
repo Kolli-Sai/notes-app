9812e188b4bf2d95fd59f01da487b431a399187d
KolliSai <saik98187@gmail.com>
2023-03-11 10:30:20 +0530

    add and delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import AppNote from "./components/AppNote";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Notes</div>;
+  return (
+    
+    <>
+      <Header />
+      <AppNote />
+    </>
+  );
 }
 
 export default App;
